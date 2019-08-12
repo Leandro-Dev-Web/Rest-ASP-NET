278af941f08f7bdd51bc44964754ae1db7a2e221
unknown <leandro.devweb@gmail.com>
2019-08-12 19:47:19 -0300

    implementing all principal verbs

---
+++ RestASPNET/Rest-ASP-NET/Model/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestASPNET.Model
+{
+    public class Pessoa
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string  SobreNome { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string Genero { get; set; }
+    }
+}
